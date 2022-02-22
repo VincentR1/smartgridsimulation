5503b4a7eaf13cff60eca3fcfb53bf3dbbe36f0b
klausschmandhoff <klausschmandhoff@gmail.com>
2022-02-22 19:54:40 +0100

    Remove error

--- src/Nodes/producer.py
+++ src/Nodes/producer.py
@@ -9,8 +9,8 @@ class Producer(Node):
         self.supply_per_step = supply_per_step
         self.sold_per_step = [0] * len(supply_per_step)
 
-    def get_balance(self, step: int) -> (float, float):
-        return BalanceReturn((self.supply_per_step[step], 0, 1, {})
+    def get_balance(self, step: int) -> BalanceReturn:
+        return BalanceReturn(self.supply_per_step[step], 0, 1, {})
 
     def settle(self, step, overflow):
         if overflow >= 0:
